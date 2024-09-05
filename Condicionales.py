f9b25dc7785e8ec7e564e598ed093618d7e73924
Niketelocreas <134002818+Niketelocreas@users.noreply.github.com>
2024-09-05 15:45:44 +0200

    Update Condicionales.py

--- Condicionales.py
+++ Condicionales.py
@@ -1,1 +1,6 @@
-# Comenzando a editar mi proyecto
+# Escribe un programa que pregunte al usuario su edad y muestre por pantalla si es mayor o menor de edad
+edad = int(input("Introduce tu edad: "))
+if edad >= 18:
+  print("Eres mayor de edad")
+else:
+  print("No eres mayor de edad")
